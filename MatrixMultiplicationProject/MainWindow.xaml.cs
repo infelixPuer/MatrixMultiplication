e4c45b89d500de82569c7bb6a17386258bc8406a
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2023-05-03 17:12:14 +0200

    Make view for fill with number option
    Also modify main view. So that there two textboxes for each matrix size input

--- MatrixMultiplicationProject/MainWindow.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MatrixMultiplicationProject;
-
-public partial class MainWindow
-{
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
-}
---
+++ MatrixMultiplicationProject/Views/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace MatrixMultiplicationProject.Views;
+
+public partial class MainWindow
+{
+    private object _fillWithNumberView;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        _fillWithNumberView = new FillWithNumberView();
+    }
+
+    private void FillWithNumberButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        ContentControl.Content = _fillWithNumberView;
+    }
+}
