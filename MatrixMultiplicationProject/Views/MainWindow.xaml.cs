fe8cfd25d1e14c61cbaa9588fe006946271b2a6d
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2023-05-03 18:11:13 +0200

    Make fill with coords product view

--- MatrixMultiplicationProject/Views/MainWindow.xaml.cs
+++ MatrixMultiplicationProject/Views/MainWindow.xaml.cs
@@ -4,14 +4,16 @@ namespace MatrixMultiplicationProject.Views;
 
 public partial class MainWindow
 {
-    private object _fillWithNumberView;
-    private object _fillWithCoordSumView;
+    private readonly object _fillWithNumberView;
+    private readonly object _fillWithCoordSumView;
+    private readonly object _fillWithCoordProductView;
 
     public MainWindow()
     {
         InitializeComponent();
         _fillWithNumberView = new FillWithNumberView();
         _fillWithCoordSumView = new FillWithCoordSumView();
+        _fillWithCoordProductView = new FillWithCoordsProductView();
     }
     private void ClearButton_OnClick(object sender, RoutedEventArgs e)
     {
@@ -27,5 +29,10 @@ public partial class MainWindow
     private void FillWithCoordsSumButton_OnClick(object sender, RoutedEventArgs e)
     {
         ContentControl.Content = _fillWithCoordSumView;
+    }
+
+    private void FillWithCoordsProductButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        ContentControl.Content = _fillWithCoordProductView;
     }
 }
