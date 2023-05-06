10b05573939266af59c7eb37ffd8db15a8c6542e
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2023-05-06 11:52:21 +0200

    Make EnterDataViewModel class

---
+++ MatrixMultiplicationProject/ViewModels/EnterDataViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MatrixMultiplicationProject.Models;
+
+namespace MatrixMultiplicationProject.ViewModels;
+
+public partial class EnterDataViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private int _firstMatrixRows;
+
+    [ObservableProperty]
+    private int _firstMatrixColumns;
+    
+    [ObservableProperty]
+    private int _secondMatrixRows;
+    
+    [ObservableProperty]
+    private int _secondMatrixColumns;
+
+    [ObservableProperty]
+    private Matrix? _firstMatrix;
+
+    [ObservableProperty]
+    private Matrix? _secondMatrix;
+
+    [RelayCommand]
+    public void ConfirmMatrixSizes()
+    {
+        FirstMatrix = new Matrix(FirstMatrixRows, FirstMatrixColumns);
+        SecondMatrix = new Matrix(SecondMatrixRows, SecondMatrixColumns);
+    }
+}
---
+++ MatrixMultiplicationTests/EnterDataViewModelTests.cs
@@ -0,0 +1,80 @@
+﻿using MatrixMultiplicationProject.ViewModels;
+
+namespace MatrixMultiplicationTests;
+
+public class EnterDataViewModelTests
+{
+    [Test]
+    public void CanInstantiateEnterDataViewModelObject()
+    {
+        var vm = new EnterDataViewModel();
+    }
+
+    [Test]
+    public void CanSetSizeForMatrices()
+    {
+        var vm = new EnterDataViewModel
+        {
+            FirstMatrixRows = 3,
+            FirstMatrixColumns = 3,
+            SecondMatrixRows = 3,
+            SecondMatrixColumns = 3
+        };
+    }
+
+    [Test]
+    public void CanConfirmMatrixSizes()
+    {
+        var vm = new EnterDataViewModel
+        {
+            FirstMatrixRows = 3,
+            FirstMatrixColumns = 3,
+            SecondMatrixRows = 3,
+            SecondMatrixColumns = 3
+        };
+
+        vm.ConfirmMatrixSizes();
+    }
+
+    [Test]
+    public void CanGetMatrices()
+    {
+        var vm = new EnterDataViewModel
+        {
+            FirstMatrixRows = 3,
+            FirstMatrixColumns = 3,
+            SecondMatrixRows = 3,
+            SecondMatrixColumns = 3
+        };
+
+        vm.ConfirmMatrixSizes();
+
+        var firstMatrix = vm.FirstMatrix;
+        var secondMatrix = vm.SecondMatrix;
+    }
+
+    [Test]
+    public void AreMatrixSizesSetsCorrectly()
+    {
+        var vm = new EnterDataViewModel
+        {
+            FirstMatrixRows = 3,
+            FirstMatrixColumns = 3,
+            SecondMatrixRows = 3,
+            SecondMatrixColumns = 3
+        };
+
+        vm.ConfirmMatrixSizes();
+
+        var firstMatrix = vm.FirstMatrix;
+        var secondMatrix = vm.SecondMatrix;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(firstMatrix.Rows, Is.EqualTo(vm.FirstMatrixRows));
+            Assert.That(firstMatrix.Columns, Is.EqualTo(vm.FirstMatrixColumns));
+            Assert.That(secondMatrix.Rows, Is.EqualTo(vm.SecondMatrixRows));
+            Assert.That(secondMatrix.Columns, Is.EqualTo(vm.SecondMatrixColumns));
+        });
+    }
+}
